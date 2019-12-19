ed50193a75649dd37c245b0900b3211d4aa2bc8c
forresthu <forresthu@gmail.com>
2019-12-19 12:38:57 -0500

    Add firebase support.  After add a project in Firebase, it has a wizard to guide you through the setup, including download the google-service.json file.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Goal
+# Goal
 Create a mobile application that assist its user to quickly identify where to recycle with the power of latest AI technology.
 
--- app/src/main/java/org/envfirst/recyclewiz/MainActivity.kt
+++ app/src/main/java/org/envfirst/recyclewiz/MainActivity.kt
@@ -1,12 +1,19 @@
 package org.envfirst.recyclewiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val options =
+            FirebaseVisionLabelDetectorOptions.Builder()
+                .setConfidenceThreshold(0.8f)
+                .build()
     }
 }
